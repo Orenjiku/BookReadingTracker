fd56c1e9936778ee01d2f41f040f73856d5c9ef8
William Chang <wdchang86@gmail.com>
2021-08-19 22:07:08 -0400

    Checkpoint before deleting node_modules and reinstalling dependencies

--- src/components/card/BookRead.tsx
+++ src/components/card/BookRead.tsx
@@ -10,12 +10,12 @@ interface BookReadPropsITF {
 
 const BookRead = ({ bookRead } : BookReadPropsITF) => {
 
-  const verticalRef = useRef(null);
-  const {refYOverflowing, refYScrollBegin, refYScrollEnd} = useOverflow(verticalRef);
+  const verticalScrollRef = useRef(null);
+  const {refYOverflowing, refYScrollBegin, refYScrollEnd} = useOverflow(verticalScrollRef);
 
   return (
     <div id='view' className='relative h-full overflow-y-hidden'>
-      <div ref={verticalRef} className='h-full overflow-y-scroll border border-red-100 scrollbar-hide'>
+      <div ref={verticalScrollRef} className='h-full overflow-y-scroll border border-red-100 scrollbar-hide'>
         <div className='flex justify-between font-Charm-400'>
           <p className='text-sm'>Days Read: {bookRead.days_read}</p>
           <p className='text-sm'>Days Total: {bookRead.days_total}</p>
