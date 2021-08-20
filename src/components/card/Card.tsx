0e71463127e4e22c850dece1aa3c5b2c57679de9
William Chang <wdchang86@gmail.com>
2021-08-20 00:28:56 -0400

    Created styled component for Card Button and added Edit and Update Progress Buttons to the Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -6,13 +6,27 @@ import DetailsSlider from './DetailsSlider';
 import BookRead from './BookRead';
 import tw from 'twin.macro';
 
-const Button = tw.button`text-purple-500`
+const Button = tw.button`
+  font-AdventPro-200
+  text-sm
+  bg-blueGray-500
+  bg-opacity-50
+  border-red-500
+  border
+  rounded
+  w-max
+  h-6
+  px-1.5
+  mx-1
+  flex
+  justify-center
+  items-center
+`
 
 const Card = ({ book }: { book: BookITF }) => {
   const totalDays = book.book_read.reduce((acc, cur) => acc + cur.days_total, 0);
   const totalDaysRead = book.book_read.reduce((acc, cur) => acc + cur.days_read, 0);
-  const avgDailyRead = Math.round(book.total_pages / totalDaysRead);
-  const timesRead = book.book_read.length;
+
   const dailyPagesRead = book.book_read.reduce((acc: number[], cur): number[] => {
     const pagesRead = cur.read_entry ? cur.read_entry.map(readEntry => readEntry.pages_read): [];
     return [...acc, ...pagesRead];
@@ -21,11 +35,11 @@ const Card = ({ book }: { book: BookITF }) => {
 
   const readDetails = [
     {key: 'Total Pages', value: book.total_pages},
-    {key: 'Avg Daily Read', value: avgDailyRead},
+    {key: 'Avg Daily Read', value: Math.round(book.total_pages / totalDaysRead)},
     {key: 'Max Daily Read', value: maxDailyRead},
     {key: 'Total Days', value: totalDays},
     {key: 'Total Days Read', value: totalDaysRead},
-    {key: 'Times Read', value: timesRead},
+    {key: 'Times Read', value: book.book_read.length},
   ];
 
   return (
@@ -39,7 +53,13 @@ const Card = ({ book }: { book: BookITF }) => {
           })
         }
       </div>
-      <Button>Edit</Button>
+      <div className='col-start-2 col-end-3 row-start-17 row-end-19 flex justify-center items-center bg-trueGray-50'>
+        <Button>Edit</Button>
+        <Button>Update Progress</Button>
+      </div>
+      <div className='col-start-1 col-end-3 row-start-19 row-end-21 flex justify-center items-center bg-blueGray-700 rounded-b-2xl font-Charm-400 text-trueGray-50 text-2xl'>
+        Completed!
+      </div>
     </div>
   )
 }
--- src/components/card/DetailsSlider.tsx
+++ src/components/card/DetailsSlider.tsx
@@ -17,7 +17,7 @@ const DetailsSlider = ({ readDetails }: DetailsSliderPropsITF) => {
   const prevSlide = () => setCurrent((current + length - 1 ) % length);
 
   return (
-    <div className='relative flex row-start-4 row-end-10 col-start-2 col-end-3 align-middle text-sm font-SortsMillGoudy-400 bg-trueGray-50 cursor-pointer'>
+    <div className='relative flex row-start-4 row-end-10 col-start-2 col-end-3 align-middle text-sm font-SortsMillGoudy-400 cursor-pointer bg-trueGray-50'>
       <div className='relative flex h-full w-full justify-center items-center z-0'>
         <div className='absolute text-7.5xl text-blue-500 text-opacity-50 z-0'>{readDetails[current].value}</div>
         <div className='absolute font-AdventPro-400 text-2xl z-0'>{readDetails[current].key}</div>
