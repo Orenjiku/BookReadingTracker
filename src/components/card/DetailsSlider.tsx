ea219e8fef27e2328a2005c6be0e7fd262d21edb
William Chang <wdchang86@gmail.com>
2021-08-25 12:21:08 -0400

    Created hover transition for DetailsSlider arrow icons. Added onClick to DetailsSlider circle icons.

--- src/components/card/DetailsSlider.tsx
+++ src/components/card/DetailsSlider.tsx
@@ -12,7 +12,11 @@ interface DetailsSliderPropsITF {
 }
 
 const ChevronContainer = styled.div<{left?: boolean; right?: boolean;}>`
-  ${tw`absolute h-full w-1/2 z-50`};
+  ${tw`absolute h-full w-1/6 cursor-pointer z-50`};
+  &:hover {
+    ${tw`bg-trueGray-50 opacity-40`};
+    transition: background-color 300ms ease-in-out, opacity 300ms ease-in-out;
+  };
   ${({ left }) => left && css`left: 0;`};
   ${({ right }) => right && css`right: 0;`};
 `
@@ -25,22 +29,22 @@ const DetailsSlider = ({ readDetails }: DetailsSliderPropsITF) => {
   const prevSlide = () => setCurrent((current + length - 1 ) % length);
 
   return (
-    <div className='relative row-start-4 row-end-10 col-start-2 col-end-3 align-middle text-sm font-SortsMillGoudy-400 cursor-pointer'>
-      {/* <div className='bg-sky-600 h-full w-full'></div> */}
+    <div className='relative flex row-start-4 row-end-10 col-start-2 col-end-3 align-middle text-sm font-SortsMillGoudy-400 cursor-default'>
       <div className='relative flex h-full w-full justify-center items-center bg-blueGray-300'>
         <div className='absolute text-7.5xl z-0 text-coolGray-50'>{readDetails[current].value}</div>
         <div className='absolute font-AdventPro-400 text-2xl text-trueGray-900'>{readDetails[current].key}</div>
-        <ChevronContainer left onClick={prevSlide}>
-          <BsChevronLeft className='absolute h-full left-0' />
-        </ChevronContainer>
-        <ChevronContainer right onClick={nextSlide}>
-          <BsChevronRight className='absolute h-full right-0' />
-        </ChevronContainer>
       </div>
+      <ChevronContainer left onClick={prevSlide}>
+        <BsChevronLeft className='absolute h-full left-0' />
+      </ChevronContainer>
+      <ChevronContainer right onClick={nextSlide}>
+        <BsChevronRight className='absolute h-full right-0' />
+      </ChevronContainer>
 
       <div className='absolute flex bottom-0 w-full justify-center mb-1.5'>
         {readDetails.map((_, i) => {
-          return (i === current) ? <BsCircleFill key={`BsCircleFill-${i}`} size={7} className='mx-0.5 fill-current text-coolGray-50' /> : <BsCircle key={`BsCircle-${i}`} size={7} className='mx-0.5' />
+          return (i === current) ? <BsCircleFill key={`BsCircleFill-${i}`} size={7} className='mx-0.5 fill-current text-coolGray-50' /> :
+          <BsCircle key={`BsCircle-${i}`} size={7} className='mx-0.5 cursor-pointer' onClick={() => setCurrent(i)} />
         })}
       </div>
 
