56b08dae0d9a12a19dad30ac7f70d7c72445db41
William Chang <wdchang86@gmail.com>
2021-08-25 15:20:25 -0400

    Created StyledBsCircleFill styled component.

--- src/components/card/DetailsSlider.tsx
+++ src/components/card/DetailsSlider.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
-import { BsCircle, BsCircleFill } from 'react-icons/bs';
-import tw, { styled, css} from 'twin.macro';
+// import { BsCircle, BsCircleFill } from 'react-icons/bs';
+import { BsCircleFill } from 'react-icons/bs';
+import tw, { styled, css } from 'twin.macro';
 
 interface DetailsSliderPropsITF {
   isUpdating: boolean;
@@ -12,14 +13,23 @@ interface DetailsSliderPropsITF {
 }
 
 const ChevronContainer = styled.div<{left?: boolean; right?: boolean;}>`
-  ${tw`absolute h-full w-1/6 cursor-pointer z-50`};
+  ${tw`absolute h-full w-1/6 cursor-pointer z-50 transition duration-300 ease-in-out`};
   &:hover {
-    ${tw`bg-trueGray-50 opacity-40`};
-    transition: background-color 300ms ease-in-out, opacity 300ms ease-in-out;
+    ${tw`bg-trueGray-50 opacity-40 transition duration-500 ease-in-out`};
   };
   ${({ left }) => left && css`left: 0;`};
   ${({ right }) => right && css`right: 0;`};
 `
+
+const StyledBsCircleFill = styled(BsCircleFill)<{selected?: boolean}>`
+  ${tw`mx-0.5 fill-current text-coolGray-50 cursor-pointer`};
+  ${({ selected }) => selected && css`${tw`fill-current text-teal-600`}`};
+  ${({ selected }) => !selected && css`
+    &:hover {
+      ${tw`fill-current text-teal-600 animate-pulse`};
+    }
+  `}
+  `
 
 const DetailsSlider = ({ readDetails }: DetailsSliderPropsITF) => {
   const [current, setCurrent] = useState<number>(0);
@@ -29,11 +39,13 @@ const DetailsSlider = ({ readDetails }: DetailsSliderPropsITF) => {
   const prevSlide = () => setCurrent((current + length - 1 ) % length);
 
   return (
-    <div className='relative flex row-start-4 row-end-10 col-start-2 col-end-3 align-middle text-sm font-SortsMillGoudy-400 cursor-default'>
+    <div className='relative flex row-start-4 row-end-10 col-start-2 col-end-3 align-middle text-sm font-SortsMillGoudy-400'>
+
       <div className='relative flex h-full w-full justify-center items-center bg-blueGray-300'>
-        <div className='absolute text-7.5xl z-0 text-coolGray-50'>{readDetails[current].value}</div>
+        <div className='absolute text-7.5xl text-coolGray-50'>{readDetails[current].value}</div>
         <div className='absolute font-AdventPro-400 text-2xl text-trueGray-900'>{readDetails[current].key}</div>
       </div>
+
       <ChevronContainer left onClick={prevSlide}>
         <BsChevronLeft className='absolute h-full left-0' />
       </ChevronContainer>
@@ -42,10 +54,11 @@ const DetailsSlider = ({ readDetails }: DetailsSliderPropsITF) => {
       </ChevronContainer>
 
       <div className='absolute flex bottom-0 w-full justify-center mb-1.5'>
-        {readDetails.map((_, i) => {
-          return (i === current) ? <BsCircleFill key={`BsCircleFill-${i}`} size={7} className='mx-0.5 fill-current text-coolGray-50' /> :
-          <BsCircle key={`BsCircle-${i}`} size={7} className='mx-0.5 cursor-pointer' onClick={() => setCurrent(i)} />
-        })}
+        {readDetails.map((_, i) => (
+          (i === current)
+          ? <StyledBsCircleFill selected key={`BsCircleFill-${i}`} size={7} />
+          : <StyledBsCircleFill key={`BsCircleFill-${i}`} size={7} onClick={() => setCurrent(i)} />
+        ))}
       </div>
 
     </div>
