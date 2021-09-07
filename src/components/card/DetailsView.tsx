37789a6bd1e7581fe267d2bf84bc19f77f65c6b9
William Chang <wdchang86@gmail.com>
2021-09-07 13:28:18 -0400

    Refactored css in styled components to tailwindcss classes.

--- src/components/card/DetailsView.tsx
+++ src/components/card/DetailsView.tsx
@@ -11,22 +11,23 @@ interface DetailsViewITF {
 }
 
 const AnimatedChevronContainer = styled.div<{left?: boolean; right?: boolean;}>`
-  ${tw`absolute h-full w-1/6 cursor-pointer transition duration-300 ease-in`};
+  ${tw`absolute h-full w-1/6 cursor-pointer`};
+  ${tw`transition duration-300 ease-in`};
   &:hover {
-    ${tw`bg-trueGray-50 bg-opacity-40 transition duration-500 ease-in-out`};
+    ${tw`bg-trueGray-50 bg-opacity-40 transition-all duration-500 ease-in-out`};
   };
-  ${({ left }) => left && css`left: 0;`};
-  ${({ right }) => right && css`right: 0;`};
+  ${({ left }) => left && css`${tw`left-0`}`};
+  ${({ right }) => right && css`${tw`right-0`}`};
 `
 
-const AnimatedBsCircleFill = styled(BsCircleFill)<{selected?: boolean}>`
+const AnimatedBsCircleFill = styled(BsCircleFill)<{selected: boolean}>`
   ${tw`mx-0.5 fill-current text-coolGray-50 cursor-pointer`};
-  ${({ selected }) => selected && css`${tw`fill-current text-teal-600`}`};
-  ${({ selected }) => !selected && css`
-    &:hover {
+  ${({ selected }) => selected
+    ? css`${tw`fill-current text-teal-600`}`
+    : css`&:hover {
       ${tw`fill-current text-teal-600 animate-pulse`};
-    }
-  `}
+    }`
+  }
 `
 
 const DetailsView = ({ readDetails }: DetailsViewITF) => {
@@ -37,23 +38,23 @@ const DetailsView = ({ readDetails }: DetailsViewITF) => {
   const prevSlide = () => setCurrent((current + length - 1 ) % length);
 
   return (
-    <div className='relative row-start-4 row-end-10 col-start-2 col-end-3 flex justify-center items-center text-sm font-SortsMillGoudy-400 bg-blueGray-300 overflow-hidden'>
+    <div className='relative row-start-4 row-end-10 col-start-2 col-end-3 flex justify-center items-center bg-blueGray-300 overflow-hidden text-sm font-SortsMillGoudy-400'>
 
-      <div className='absolute text-7.5xl text-coolGray-50'>{readDetails[current].value}</div>
-      <div className='absolute font-AdventPro-400 text-2xl text-trueGray-900'>{readDetails[current].key}</div>
+      <div className='absolute text-coolGray-50 text-7.5xl'>{readDetails[current].value}</div>
+      <div className='absolute text-trueGray-900 text-2xl font-AdventPro-400'>{readDetails[current].key}</div>
 
-      <AnimatedChevronContainer left onClick={prevSlide}>
+      <AnimatedChevronContainer left onClick={() => prevSlide()}>
         <BsChevronLeft className='absolute h-full left-0' />
       </AnimatedChevronContainer>
-      <AnimatedChevronContainer right onClick={nextSlide}>
+      <AnimatedChevronContainer right onClick={() => nextSlide()}>
         <BsChevronRight className='absolute h-full right-0' />
       </AnimatedChevronContainer>
 
-      <div className='absolute flex bottom-0 w-full justify-center mb-1.5'>
+      <div className='absolute bottom-0 w-full mb-1.5 flex justify-center'>
         {readDetails.map((_, i) => (
           (i === current)
-          ? <AnimatedBsCircleFill selected key={`BsCircleFill-${i}`} size={7} />
-          : <AnimatedBsCircleFill key={`BsCircleFill-${i}`} size={7} onClick={() => setCurrent(i)} />
+          ? <AnimatedBsCircleFill key={`BsCircleFill-${i}`} size={7} selected={true} />
+          : <AnimatedBsCircleFill key={`BsCircleFill-${i}`} size={7} selected={false} onClick={() => setCurrent(i)} />
         ))}
       </div>
 
