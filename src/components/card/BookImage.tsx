018a1495c77baae9fa53f3d277034fd0edd5c34c
William Chang <wdchang86@gmail.com>
2021-09-07 10:10:33 -0400

    Converted Button styled component css properties to tailwindcss classes.

--- src/components/card/BookImage.tsx
+++ src/components/card/BookImage.tsx
@@ -3,32 +3,29 @@ import tw, { styled } from 'twin.macro';
 import { CSSTransition } from 'react-transition-group';
 
 const Button = styled.div<{isUpdating: boolean}>`
-  ${tw`h-8 w-24 flex justify-center items-center bg-blueGray-300 bg-opacity-40 filter backdrop-filter backdrop-blur rounded cursor-pointer text-trueGray-50 font-Charm-400`};
-  transition: background-color 300ms linear;
+  ${tw`h-8 w-24 flex justify-center items-center rounded cursor-pointer`};
+  ${tw`bg-blueGray-300 bg-opacity-40 text-trueGray-50 font-Charm-400`};
+  ${tw`filter backdrop-filter backdrop-blur`};
+  ${tw`transition-colors duration-300 ease-linear`}
   &:hover {
-    ${tw`bg-blueGray-400 bg-opacity-40`};
-    transition: background-color 300ms linear;
+    ${tw`bg-blueGray-400 bg-opacity-40`}
+    ${tw`transition-colors duration-300 ease-linear`};
   }
   &.fade-enter {
-    opacity: 0;
+    ${tw`opacity-0`}
   }
   &.fade-enter-active {
-    opacity: 1;
-    transition: opacity 600ms ease-in-out;
-  }
-  &.fade-exit {
-    opacity: 1;
+    ${tw`opacity-100 transition-opacity duration-500 ease-in-out`}
   }
   &.fade-exit-active {
-    opacity: 0;
-    transition: opacity 600ms ease-in-out;
+    ${tw`opacity-0 transition-opacity duration-500 ease-in-out`}
   }
 `
 
 const BookImage = ({ pictureLink, isUpdating, handleFlip }: { pictureLink: string; isUpdating: boolean; handleFlip: Function }) => {
   return (
-    <div className='col-start-1 col-end-2 row-start-4 row-end-19 grid place-content-center overflow-hidden bg-cover rounded-tl-2xl' style={{backgroundImage: `url(${pictureLink})`}}>
-      <CSSTransition in={isUpdating} timeout={600} classNames='fade' unmountOnExit>
+    <div className='col-start-1 col-end-2 row-start-4 row-end-19 grid place-content-center rounded-tl-2xl bg-cover overflow-hidden' style={{backgroundImage: `url(${pictureLink})`}}>
+      <CSSTransition in={isUpdating} timeout={500} classNames='fade' unmountOnExit>
         <Button isUpdating={isUpdating} onClick={() => handleFlip()}>{isUpdating && <p>Edit Book</p>}</Button>
       </CSSTransition>
     </div>
