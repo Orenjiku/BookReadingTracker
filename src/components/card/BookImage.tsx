7204ec321ef8d182b78ea432306112380eb2f7b7
William Chang <wdchang86@gmail.com>
2021-09-07 13:33:28 -0400

    Changed Button styled component name to EditBookButton.

--- src/components/card/BookImage.tsx
+++ src/components/card/BookImage.tsx
@@ -8,31 +8,31 @@ interface BookImagePropsITF {
   handleFlip: Function;
 }
 
-const Button = styled.div<{isUpdating: boolean}>`
-  ${tw`h-8 w-24 flex justify-center items-center rounded cursor-pointer`};
+const EditBookButton = styled.button<{isUpdating: boolean}>`
+  ${tw`h-8 w-24 flex justify-center items-center rounded`};
   ${tw`bg-blueGray-300 bg-opacity-40 text-trueGray-50 font-Charm-400`};
   ${tw`filter backdrop-filter backdrop-blur`};
-  ${tw`transition-colors duration-300 ease-linear`}
+  ${tw`transition-colors duration-300 ease-linear`};
   &:hover {
-    ${tw`bg-blueGray-400 bg-opacity-40`}
+    ${tw`bg-blueGray-400 bg-opacity-40`};
     ${tw`transition-colors duration-300 ease-linear`};
   }
   &.fade-enter {
-    ${tw`opacity-0`}
+    ${tw`opacity-0`};
   }
   &.fade-enter-active {
-    ${tw`opacity-100 transition-opacity duration-500 ease-in-out`}
+    ${tw`opacity-100 transition-opacity duration-500 ease-in-out`};
   }
   &.fade-exit-active {
-    ${tw`opacity-0 transition-opacity duration-500 ease-in-out`}
+    ${tw`opacity-0 transition-opacity duration-500 ease-in-out`};
   }
 `
 
 const BookImage = ({ pictureLink, isUpdating, handleFlip }: BookImagePropsITF) => {
   return (
-    <div className='col-start-1 col-end-2 row-start-4 row-end-19 grid place-content-center rounded-tl-2xl bg-cover overflow-hidden' style={{backgroundImage: `url(${pictureLink})`}}>
+    <div className='col-start-1 col-end-2 row-start-4 row-end-19 rounded-tl-2xl grid place-content-center bg-cover overflow-hidden' style={{backgroundImage: `url(${pictureLink})`}}>
       <CSSTransition in={isUpdating} timeout={500} classNames='fade' unmountOnExit>
-        <Button isUpdating={isUpdating} onClick={() => handleFlip()}>{isUpdating && <span>Edit Book</span>}</Button>
+        <EditBookButton isUpdating={isUpdating} onClick={() => handleFlip()}>{isUpdating && <span>Edit Book</span>}</EditBookButton>
       </CSSTransition>
     </div>
   )
