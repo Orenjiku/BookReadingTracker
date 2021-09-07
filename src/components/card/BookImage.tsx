e277626aff2861f7e8aaac2c20f628d179f54b1d
William Chang <wdchang86@gmail.com>
2021-09-07 10:17:31 -0400

    Created interface for BookImage props.

--- src/components/card/BookImage.tsx
+++ src/components/card/BookImage.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import tw, { styled } from 'twin.macro';
 import { CSSTransition } from 'react-transition-group';
 
+interface BookImagePropsITF {
+  pictureLink: string;
+  isUpdating: boolean;
+  handleFlip: Function;
+}
+
 const Button = styled.div<{isUpdating: boolean}>`
   ${tw`h-8 w-24 flex justify-center items-center rounded cursor-pointer`};
   ${tw`bg-blueGray-300 bg-opacity-40 text-trueGray-50 font-Charm-400`};
@@ -22,11 +28,11 @@ const Button = styled.div<{isUpdating: boolean}>`
   }
 `
 
-const BookImage = ({ pictureLink, isUpdating, handleFlip }: { pictureLink: string; isUpdating: boolean; handleFlip: Function }) => {
+const BookImage = ({ pictureLink, isUpdating, handleFlip }: BookImagePropsITF) => {
   return (
     <div className='col-start-1 col-end-2 row-start-4 row-end-19 grid place-content-center rounded-tl-2xl bg-cover overflow-hidden' style={{backgroundImage: `url(${pictureLink})`}}>
       <CSSTransition in={isUpdating} timeout={500} classNames='fade' unmountOnExit>
-        <Button isUpdating={isUpdating} onClick={() => handleFlip()}>{isUpdating && <p>Edit Book</p>}</Button>
+        <Button isUpdating={isUpdating} onClick={() => handleFlip()}>{isUpdating && <span>Edit Book</span>}</Button>
       </CSSTransition>
     </div>
   )
