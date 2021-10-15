1743b2500de6409c554826b7910e3da83be1d4b2
William Chang <wdchang86@gmail.com>
2021-10-15 12:18:08 -0400

    Add conditional display when no picture_url provided to BookImage component.

--- src/components/card/BookImage.tsx
+++ src/components/card/BookImage.tsx
@@ -1,46 +1,55 @@
 import React, { useRef } from 'react';
 import tw, { styled } from 'twin.macro';
 import { CSSTransition } from 'react-transition-group';
+import { FiSlash } from 'react-icons/fi';
 
 
 interface BookImagePropsITF {
-  pictureLink: string;
+  pictureUrl: string;
   isEdit: boolean;
   editTimer: number;
   handleFlip: Function;
 }
 
 const EditBookButton = styled.button<{ $editTimer: number }>`
-  ${tw`h-auto w-auto py-1 px-4 mx-2 rounded border border-coolGray-50 flex justify-center items-center`};
+  ${tw`py-1 px-4 rounded border border-coolGray-50`};
   ${tw`bg-blueGray-300 bg-opacity-40 text-trueGray-50 font-Charm-400`};
   ${tw`backdrop-filter backdrop-blur`};
-  --duration: ${({ $editTimer }) => `${$editTimer}ms`};
-  transition: opacity 500ms linear;
+  --editDuration: ${({ $editTimer }) => `${$editTimer}ms`};
+  --hoverDuration: 200ms;
+  transition: all var(--hoverDuration) linear;
   &:hover {
     ${tw`bg-blueGray-400 bg-opacity-40`};
-    ${tw`transition-colors duration-300 ease-linear`};
   }
   &.fade-enter {
     opacity: 0;
   }
   &.fade-enter-active {
     opacity: 1;
-    transition: opacity var(--duration) ease-in-out;
+    transition: opacity var(--editDuration) ease-in-out;
   }
   &.fade-exit-active {
     opacity: 0;
-    transition: opacity var(--duration) ease-in-out;
+    transition: opacity var(--editDuration) ease-in-out;
   }
 `;
 
-const BookImage = ({ pictureLink, isEdit, editTimer, handleFlip }: BookImagePropsITF) => {
+const BookImage = ({ pictureUrl, isEdit, editTimer, handleFlip }: BookImagePropsITF) => {
   const bookImageRef = useRef(null);
 
   return (
-    <div className='col-start-1 col-end-2 row-start-4 row-end-20 rounded-tl-2xl grid place-content-center bg-cover bg-center overflow-hidden' style={{backgroundImage: `url(${pictureLink})`}}>
-      <CSSTransition in={isEdit} timeout={editTimer} classNames='fade' nodeRef={bookImageRef} unmountOnExit>
-        <EditBookButton ref={bookImageRef} $editTimer={editTimer} onClick={() => handleFlip()}>Edit Book</EditBookButton>
-      </CSSTransition>
+    <div className='col-start-1 col-end-2 row-start-4 row-end-20 rounded-tl-2xl'>
+      {pictureUrl === ''
+      ? <div className='h-full w-full flex flex-col justify-center items-center border-t border-r border-blueGray-50'>
+          <FiSlash className='text-red-500 opacity-40' size={75} />
+          <p className='font-AdventPro-400 text-xl opacity-60'>No Picture</p>
+        </div>
+      : <div className='h-full w-full flex justify-center items-center bg-cover bg-center' style={{backgroundImage: `url(${pictureUrl})`}}>
+          <CSSTransition in={isEdit} timeout={editTimer} classNames='fade' nodeRef={bookImageRef} unmountOnExit>
+            <EditBookButton ref={bookImageRef} $editTimer={editTimer} onClick={() => handleFlip()}>Edit Book</EditBookButton>
+          </CSSTransition>
+        </div>
+      }
     </div>
   )
 }
