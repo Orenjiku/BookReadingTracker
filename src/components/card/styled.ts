ef346e4ece3892247a6fe0604ecb3aaa90033e1f
William Chang <wdchang86@gmail.com>
2021-11-08 10:53:44 -0500

    Added hover effects on StyledButton to indicate when mouse is over button.

--- src/components/card/styled.ts
+++ src/components/card/styled.ts
@@ -31,20 +31,24 @@ const StyledButton = styled.button`
   ${tw`h-auto w-auto py-0.5 px-4 mx-1 rounded border border-coolGray-50 flex justify-center items-center overflow-hidden`};
   ${tw`bg-blueGray-300 bg-opacity-40 font-Charm-400`};
   ${tw`backdrop-filter backdrop-blur select-none`};
+  &:hover {
+    ${tw`bg-blueGray-400 bg-opacity-30`};
+  }
   &:active {
-    ${tw`bg-trueGray-400 bg-opacity-40`};
+    ${tw`bg-blueGray-400 bg-opacity-50`};
   }
 `;
 
+//SaveButton active bg color should match StyledButton hover bg color to prevent bg color change on active. On hold slide bar will indicate active.
 const SaveButton = styled(StyledButton)<{$isStartSubmit?: boolean; $submitHoldTimer?: number}>`
-  &:active{
-    ${tw`bg-blueGray-300 bg-opacity-40`};
+  &:active {
+    ${tw`bg-blueGray-400 bg-opacity-30`};
   }
   &::after {
     content: '';
     ${tw`absolute h-full w-0 left-0 bg-teal-500 bg-opacity-40`};
     z-index: -1;
-    transition: all 50ms linear;
+    transition: all 100ms linear;
     ${({ $isStartSubmit, $submitHoldTimer }) => $isStartSubmit && css`
       ${tw`h-full w-full`};
       transition: all ${$submitHoldTimer}ms linear;
