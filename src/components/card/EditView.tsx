8765b9cf582c108479e542abfa4a44a36ed444af
William Chang <wdchang86@gmail.com>
2021-11-10 10:36:12 -0500

    Added indication and transition effects on ReadInstanceEditSelectContainer component on hover.

--- src/components/card/EditView.tsx
+++ src/components/card/EditView.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import tw, { styled, css } from 'twin.macro';
 import useHoldSubmit from '../../hooks/useHoldSubmit';
+import useDelayReset from '../../hooks/useDelayReset';
 import { StyledButton } from './styled';
 import { isValidDate } from './utils';
 import FormLabel from './FormLabel';
@@ -66,14 +67,23 @@ const ReadInstanceEditSelectContainer = styled.div`
   }
   `;
 
-const StyledBsCircle = styled(BsCircleFill)<{ $isStartSubmit: boolean, $submitHoldTimer: number}>`
-  ${tw`absolute bg-blueGray-900 opacity-0`};
+const StyledBsCircle = styled(BsCircleFill)<{ $inner?: boolean, $outer?: boolean, $isStartSubmit: boolean, $submitHoldTimer: number}>`
+  ${tw`absolute opacity-0`}
   --holdDuration: ${({ $submitHoldTimer}) => `${$submitHoldTimer}ms`};
-  height: 200px;
-  width: 200px;
-  border-radius: 50%;
-  z-index: -1;
-  transform-origin: center;
+  ${({ $inner }) => $inner && css`
+    ${tw`absolute bg-blueGray-400 bg-opacity-30`}
+    height: 120px;
+    width: 120px;
+    border-radius: 50%;
+    z-index: -1;
+  `}
+  ${({ $outer }) => $outer && css`
+    ${tw`absolute bg-blueGray-900`};
+    height: 200px;
+    width: 200px;
+    border-radius: 50%;
+    z-index: -2;
+  `}
   ${({ $isStartSubmit }) => $isStartSubmit && css`
     ${tw`opacity-40`};
     transform: scale(.1);
@@ -81,65 +91,118 @@ const StyledBsCircle = styled(BsCircleFill)<{ $isStartSubmit: boolean, $submitHo
   `}
 `;
 
+const StyledChevronDown = styled(CgPushChevronDownR)`
+  ${tw`ml-0.5 text-blueGray-50`};
+  transition: transform 200ms ease-out;
+  ${ReadInstanceEditSelectContainer}:hover & {
+    ${tw`text-blueGray-900`}
+    transform: translateY(25%);
+    --neon-light-center: #f9fafb;
+    --neon-light-color: #0d9488;
+    --light-effect: drop-shadow(0 0 4px var(--neon-light-center))
+                    drop-shadow(0 0 6px var(--neon-light-center))
+                    drop-shadow(0 0 16px var(--neon-light-color));
+    color: var(--neon-light-center);
+    filter: var(--light-effect);
+    fill: none;
+    transition: filter 200ms transform 200ms, linear;
+  }
+`
+
 const EditView = ({ readerBookId, readInstanceId, totalPages, isEdit, editTimer, isFlipped, flipTimer, handleUpdateReaderBook }: EditViewPropsITF) => {
   const [ readEntryDate, setReadEntryDate ] = useState(new Date(Date.now()).toISOString().slice(0, 10));
   const [ readEntryCurrentPage, setReadEntryCurrentPage ] = useState('');
 
-  const [ isSubmitReadEntrySuccess, setIsSubmitReadEntrySuccess ] = useState(false);
-  const [ isSubmitReadEntryFail, setIsSubmitReadEntryFail ] = useState(false);
+  const [ isSubmitDateSuccess, setIsSubmitDateSuccess ] = useState(false);
+  const [ isSubmitDateFail, setIsSubmitDateFail ] = useState(false);
 
-  const toggleReadEntrySubmitSuccessState = () => {
-    setIsSubmitReadEntrySuccess(true);
-    setIsSubmitReadEntryFail(false);
+  const [ isSubmitPageSuccess, setIsSubmitPageSuccess ] = useState(false);
+  const [ isSubmitPageFail, setIsSubmitPageFail ] = useState(false);
+
+  const [ isReadEntryEditView, setIsReadEntryEditView ] = useState(true);
+  const slideTimer = 300;
+  const readEntryEditContainerRef = useRef(null);
+  const readInstanceEditContainerRef = useRef(null);
+
+  const handleChangeEditView = () => setIsReadEntryEditView(prevIsReadEntryEditView => !prevIsReadEntryEditView);
+
+  const toggleSubmitDateSuccessState = () => {
+    setIsSubmitDateSuccess(true);
+    setIsSubmitDateFail(false);
   };
 
-  const toggleReadEntrySubmitFailState = () => {
-    setIsSubmitReadEntrySuccess(false);
-    setIsSubmitReadEntryFail(true);
+  const toggleSubmitDateFailState = () => {
+    setIsSubmitDateSuccess(false);
+    setIsSubmitDateFail(true);
   };
 
-  const resetReadEntrySubmitStates = () => {
-    setIsSubmitReadEntrySuccess(false);
-    setIsSubmitReadEntryFail(false);
+  const toggleSubmitPageSuccessState = () => {
+    setIsSubmitPageSuccess(true);
+    setIsSubmitPageFail(false);
   };
 
-  //clear inputs ad submit status when edit state is changed to false.
+  const toggleSubmitPageFailState = () => {
+    setIsSubmitPageSuccess(false);
+    setIsSubmitPageFail(true);
+  };
+
+  const resetSubmitDateStates = () => {
+    setIsSubmitDateSuccess(false);
+    setIsSubmitDateFail(false);
+  };
+
+  const resetSubmitPageStates = () => {
+    setIsSubmitPageSuccess(false);
+    setIsSubmitPageFail(false);
+  };
+
+  const resetAllStates = () => {
+    setIsReadEntryEditView(true);
+    setReadEntryDate(new Date(Date.now()).toISOString().slice(0, 10));
+    setReadEntryCurrentPage('');
+    resetSubmitDateStates();
+    resetSubmitPageStates();
+  }
+
+  useDelayReset(isEdit, false, editTimer, resetAllStates);
+  useDelayReset(isFlipped, true, flipTimer / 2, resetAllStates);
+
+  //clear inputs and submit status when page change
   useEffect(() => {
     let delayTimeout: ReturnType<typeof setTimeout>;
-    if (!isEdit) {
+    if (!isReadEntryEditView) {
       delayTimeout = setTimeout(() => {
-        setReadEntryDate(new Date(Date.now()).toISOString().slice(0, 10));
-        setReadEntryCurrentPage('');
-        resetReadEntrySubmitStates();
-      }, editTimer);
+        resetSubmitDateStates();
+        resetSubmitPageStates();
+      }, slideTimer);
     }
     () => clearTimeout(delayTimeout);
-  }, [isEdit]);
-  //---
-
-  //clear inputs and submit status when card is flipped.
-  useEffect(() => {
-    let delayTimeout: ReturnType<typeof setTimeout>;
-    if (isFlipped) {
-      delayTimeout = setTimeout(() => {
-        setReadEntryDate(new Date(Date.now()).toISOString().slice(0, 10));
-        setReadEntryCurrentPage('');
-        resetReadEntrySubmitStates();
-      }, flipTimer / 2);
-    }
-    () => clearTimeout(delayTimeout);
-  }, [isFlipped]);
+  }, [isReadEntryEditView]);
   //---
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === 'readEntryDate') setReadEntryDate(e.target.value);
-    else if (e.target.name === 'readEntryCurrentPage') setReadEntryCurrentPage(e.target.value);
-    resetReadEntrySubmitStates();
+    if (e.target.name === 'readEntryDate') {
+      setReadEntryDate(e.target.value);
+      resetSubmitDateStates();
+      isSubmitPageSuccess && resetSubmitPageStates();
+    }
+    else if (e.target.name === 'readEntryCurrentPage') {
+      setReadEntryCurrentPage(e.target.value);
+      resetSubmitPageStates();
+      isSubmitDateSuccess && resetSubmitDateStates();
+    }
   };
 
   const handleSubmitReadEntry = async () => {
     const dateString = `${readEntryDate} ${new Date().toTimeString().slice(0, 8)}`;
-    if (isValidDate(readEntryDate) && readEntryCurrentPage !== '') {
+    if (!isValidDate(readEntryDate) && readEntryCurrentPage === '') {
+      toggleSubmitDateFailState();
+      toggleSubmitPageFailState();
+    } else if (!isValidDate(readEntryDate) && readEntryCurrentPage !== '') {
+      toggleSubmitDateFailState();
+    } else if (isValidDate(readEntryDate) && readEntryCurrentPage === '') {
+      toggleSubmitPageFailState();
+    } else if (isValidDate(readEntryDate) && readEntryCurrentPage !== '') {
       try {
         const response = await fetch('http://localhost:3000/1/book/read_entry', {
           method: 'POST',
@@ -147,21 +210,24 @@ const EditView = ({ readerBookId, readInstanceId, totalPages, isEdit, editTimer,
           body: JSON.stringify({ readerBookId, readInstanceId, dateString, currentPage: Number(readEntryCurrentPage), totalPages })
         });
         if (response.ok) {
-          toggleReadEntrySubmitSuccessState();
           const result = await response.json();
           handleUpdateReaderBook(result);
+          setReadEntryCurrentPage('');
+          toggleSubmitDateSuccessState();
+          toggleSubmitPageSuccessState();
         } else {
-          toggleReadEntrySubmitFailState();
+          toggleSubmitDateFailState();
+          toggleSubmitPageFailState();
         }
       } catch(err) {
         console.error(err);
-        toggleReadEntrySubmitFailState();
+        toggleSubmitDateFailState();
+        toggleSubmitPageFailState();
       }
-    } else {
-      readEntryCurrentPage.trim() === '' && setReadEntryCurrentPage(''); //reset text cursor if value is a string of only spaces.
-      toggleReadEntrySubmitFailState();
     }
   };
+
+  const handleSubmitReadEntryWithEnter = (e: React.KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && handleSubmitReadEntry();
 
   const handlePostReadInstance = async () => {
     try {
@@ -195,57 +261,52 @@ const EditView = ({ readerBookId, readInstanceId, totalPages, isEdit, editTimer,
     }
   }
 
-  const [ isDefaultPage, setIsDefaultPage ] = useState(true);
-  const slideTimer = 300;
-  const readEntryEditContainerRef = useRef(null);
-  const readInstanceEditContainerRef = useRef(null);
-
-  const handleChangePage = () => setIsDefaultPage(prevIsDefaultPage => !prevIsDefaultPage);
-
-  const submitPostHoldTimer = 500;
+  const submitPostHoldTimer = 600;
   const submitDeleteHoldTimer = 1000;
   const [ isStartPostSubmit, handleStartPostSubmit, handleStopPostSubmit ] = useHoldSubmit(submitPostHoldTimer, handlePostReadInstance);
   const [ isStartDeleteSubmit, handleStartDeleteSubmit, handleStopDeleteSubmit ] = useHoldSubmit(submitDeleteHoldTimer, handleDeleteReadInstance);
 
   return (
     <div className='relative h-full w-full'>
-      <CSSTransition in={isDefaultPage} timeout={slideTimer} classNames='slide' nodeRef={readEntryEditContainerRef} unmountOnExit>
+      <CSSTransition in={isReadEntryEditView} timeout={slideTimer} classNames='slide' nodeRef={readEntryEditContainerRef} unmountOnExit>
         <ReadEntryEditContainer $slideTimer={slideTimer} ref={readEntryEditContainerRef}>
           <form className='col-start-1 col-end-11 ml-2 mr-1 flex flex-col justify-center items-center'>
-            <FormLabel type='text' label='Date' name='readEntryDate' value={readEntryDate} placeholder='yyyy-mm-dd' submitStatus={[isSubmitReadEntrySuccess, isSubmitReadEntryFail]} feedbackText='' handleInputChange={handleInputChange} />
+            <FormLabel type='text' label='Date' name='readEntryDate' value={readEntryDate} placeholder='yyyy-mm-dd' submitStatus={[isSubmitDateSuccess, isSubmitDateFail]} feedbackText='' handleInputChange={handleInputChange}  handleEnter={handleSubmitReadEntryWithEnter} />
 
             <div className='flex'>
-              <FormLabel type='number' label='Page' name='readEntryCurrentPage' value={readEntryCurrentPage} placeholder='#' submitStatus={[isSubmitReadEntrySuccess, isSubmitReadEntryFail]} feedbackText='' handleInputChange={handleInputChange} />
+              <FormLabel type='number' label='Page' name='readEntryCurrentPage' value={readEntryCurrentPage} placeholder='#' submitStatus={[isSubmitPageSuccess, isSubmitPageFail]} feedbackText='' handleInputChange={handleInputChange} handleEnter={handleSubmitReadEntryWithEnter} />
               <div className='mb-0.5 ml-1 flex items-end'>
                 <StyledButton type='button' onClick={handleSubmitReadEntry}>Add</StyledButton>
               </div>
             </div>
           </form>
 
-          <div className='col-start-11 col-end-13 flex justify-center items-center cursor-pointer hover:bg-blueGray-400 hover:bg-opacity-30' onClick={handleChangePage}>
+          <div className='col-start-11 col-end-13 flex justify-center items-center cursor-pointer hover:bg-blueGray-400 hover:bg-opacity-30' onClick={handleChangeEditView}>
             <BsChevronRight className='absolute right-0 stroke-current stroke-1 text-coolGray-50' />
           </div>
         </ReadEntryEditContainer>
       </CSSTransition>
 
-      <CSSTransition in={!isDefaultPage} timeout={slideTimer} classNames='slide' nodeRef={readInstanceEditContainerRef} unmountOnExit>
+      <CSSTransition in={!isReadEntryEditView} timeout={slideTimer} classNames='slide' nodeRef={readInstanceEditContainerRef} unmountOnExit>
         <ReadInstanceEditContainer $slideTimer={slideTimer} ref={readInstanceEditContainerRef}>
-          <div className='col-start-1 col-end-3 flex justify-center items-center cursor-pointer hover:bg-blueGray-400 hover:bg-opacity-30' onClick={handleChangePage}>
+          <div className='col-start-1 col-end-3 flex justify-center items-center cursor-pointer hover:bg-blueGray-400 hover:bg-opacity-30' onClick={handleChangeEditView}>
             <BsChevronLeft className='absolute left-0 stroke-current stroke-1 text-coolGray-50' />
           </div>
+
           <div className='col-start-3 col-end-13 flex flex-col'>
             <ReadInstanceEditSelectContainer onMouseDown={() => handleStartPostSubmit()} onMouseUp={() => handleStopPostSubmit()} onMouseLeave={() => handleStopPostSubmit()}>
               <p>New Read</p>
-              <CgPushChevronDownR className='current-stroke text-blueGray-700' />
-              <StyledBsCircle $isStartSubmit={isStartPostSubmit} $submitHoldTimer={submitPostHoldTimer} />
+              <StyledChevronDown />
+              <StyledBsCircle $inner $isStartSubmit={isStartPostSubmit} $submitHoldTimer={submitPostHoldTimer} />
+              <StyledBsCircle $outer $isStartSubmit={isStartPostSubmit} $submitHoldTimer={submitPostHoldTimer} />
             </ReadInstanceEditSelectContainer>
             <ReadInstanceEditSelectContainer onMouseDown={() => handleStartDeleteSubmit()} onMouseUp={() => handleStopDeleteSubmit()} onMouseLeave={() => handleStopDeleteSubmit()}>
               <p>Delete Read</p>
-              <CgPushChevronDownR className='current-stroke text-blueGray-700' />
-              <StyledBsCircle $isStartSubmit={isStartDeleteSubmit} $submitHoldTimer={submitDeleteHoldTimer} />
+              <StyledChevronDown />
+              <StyledBsCircle $inner $isStartSubmit={isStartDeleteSubmit} $submitHoldTimer={submitDeleteHoldTimer} />
+              <StyledBsCircle $outer $isStartSubmit={isStartDeleteSubmit} $submitHoldTimer={submitDeleteHoldTimer} />
             </ReadInstanceEditSelectContainer>
           </div>
-
         </ReadInstanceEditContainer>
       </CSSTransition>
     </div>
