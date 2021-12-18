e1a646304be26940af317f06bc0412a7452feb70
William Chang <wdchang86@gmail.com>
2021-12-18 12:19:34 -0500

    Updated slide transition to move in the correct direction when adding and deleting readInstances in ReaderBook.

--- src/components/card/ReaderBook.tsx
+++ src/components/card/ReaderBook.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, cloneElement } from 'react';
 import tw, { styled } from 'twin.macro';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { ReadInstanceITF } from '../../interfaces/interface';
+import usePrevious from '../../hooks/usePrevious';
 import ReadInstance from './ReadInstance';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 
@@ -59,27 +60,34 @@ const ReadInstanceContainer = styled.div`
 `;
 
 const ReaderBook = ({ readInstanceList, readInstanceIdx, isEdit, editTimer, isExpanded, expandTimer, handleIsExpanded, handleChangeReadInstanceIdx, handleUpdateReaderBook }: ReaderBookPropsITF) => {
-  const readInstanceListLength = readInstanceList.length;
+  const [ currLen, setCurrLen ] = useState(readInstanceList.length);
+  const prevLen = usePrevious(currLen);
+
+  const [ currIdx, setCurrIdx ] = useState(readInstanceIdx);
+  const prevIdx = usePrevious(currIdx);
 
   const [ isIdxStart, setIsIdxStart ] = useState(true);
   const [ isIdxEnd, setIsIdxEnd ] = useState(false);
 
-  const [ transitionClassNames, setTransitionClassNames ] = useState('');
-  // const readerBookRef = useRef(null);
+  //updating classNames inside useEffect doesn't seem to update before transition. Outside useEffect, classNames generates correct transition.
+  const classNames = readInstanceList.length < prevLen || readInstanceIdx < prevIdx || (readInstanceIdx === 0 && readInstanceList.length > prevLen) ? 'backward' : 'forward';
 
   useEffect(() => {
+    //if idx at 0, remove left arrow
     readInstanceIdx === 0 ? setIsIdxStart(true) : setIsIdxStart(false);
-    readInstanceIdx === readInstanceListLength - 1 ? setIsIdxEnd(true) : setIsIdxEnd(false);
+    //if idx at last, remove right arrow.
+    readInstanceIdx === readInstanceList.length - 1 ? setIsIdxEnd(true) : setIsIdxEnd(false);
+    //only purpose in updating currLen and currIdx is to update prevLen and prevIdx states used in determining classNames value.
+    setCurrLen(readInstanceList.length);
+    setCurrIdx(readInstanceIdx);
   }, [readInstanceList, readInstanceIdx]);
 
   const prevSlide = () => {
-    handleChangeReadInstanceIdx(readInstanceIdx - 1);
-    setTransitionClassNames('backward');
+    handleChangeReadInstanceIdx(currIdx - 1);
   };
 
   const nextSlide = () => {
-    handleChangeReadInstanceIdx(readInstanceIdx + 1);
-    setTransitionClassNames('forward');
+    handleChangeReadInstanceIdx(currIdx + 1);
   };
 
   const convertToRoman = (num: number): string | number => {
@@ -91,7 +99,7 @@ const ReaderBook = ({ readInstanceList, readInstanceIdx, isEdit, editTimer, isEx
     <div className='h-full overflow-hidden'>
 
       <ReaderBookHeader>
-        <p className='font-Alegreya-500 text-center text-xl'>{convertToRoman(readInstanceListLength - readInstanceIdx)}</p>
+        <p className='font-Alegreya-500 text-center text-xl'>{convertToRoman(readInstanceList.length - readInstanceIdx)}</p>
 
         {!isIdxStart &&
           <div className='absolute left-0 w-1/3 h-full flex items-center cursor-pointer' onClick={() => prevSlide()} >
@@ -106,7 +114,7 @@ const ReaderBook = ({ readInstanceList, readInstanceIdx, isEdit, editTimer, isEx
         }
       </ReaderBookHeader>
 
-      <TransitionGroup component={null} childFactory={child => cloneElement(child, {classNames: transitionClassNames})}>
+      <TransitionGroup component={null} childFactory={child => cloneElement(child, {classNames})}>
         <CSSTransition key={`ReadInstance-${readInstanceIdx}`} timeout={300} unmountOnExit /* nodeRef={readerBookRef} */>
           <ReadInstanceContainer /* ref={readerBookRef} */>
             <ReadInstance key={readInstanceList[readInstanceIdx].ri_id} readInstance={readInstanceList[readInstanceIdx]} isEdit={isEdit} editTimer={editTimer} isExpanded={isExpanded} expandTimer={expandTimer} handleIsExpanded={handleIsExpanded} handleUpdateReaderBook={handleUpdateReaderBook}/>
