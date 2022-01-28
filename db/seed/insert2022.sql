70d8f6c577b3655e38db0f2559836941681ea9e0
William Chang <wdchang86@gmail.com>
2022-01-28 17:28:23 -0500

    Added new books.

--- db/seed/insert2022.sql
+++ db/seed/insert2022.sql
@@ -45,12 +45,43 @@
 \set book_5_blurb 'Murderbot returns in its highly-anticipated, first, full-length standalone novel. \n\nYou know that feeling when you’re at work, and you’ve had enough of people, and then the boss walks in with yet another job that needs to be done right this second or the world will end, but all you want to do is go home and binge your favorite shows? And you''re a sentient murder machine programmed for destruction? Congratulations, you''re Murderbot. \n\nCome for the pew-pew space battles, stay for the most relatable A.I. you’ll read this century. \n\n— \n\nI’m usually alone in my head, and that’s where 90 plus percent of my problems are. \n\nWhen Murderbot''s human associates (not friends, never friends) are captured and another not-friend from its past requires urgent assistance, Murderbot must choose between inertia and drastic action. \n\nDrastic action it is, then.'
 \set book_5_book_cover_url 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1640597293l/52381770._SY475_.jpg'
 
+\set book_6_title 'Fugitive Telemetry'
+\set book_6_title_sort 'fugitive telemetry'
+\set book_6_published_date '2021/05/27'
+\set book_6_edition_date '2021/05/27'
+\set book_6_book_format 'Hardcover'
+\set book_6_total_pages 168
+\set book_6_blurb 'No, I didn’t kill the dead human. If I had, I wouldn’t dump the body in the station mall. \n\nWhen Murderbot discovers a dead body on Preservation Station, it knows it is going to have to assist station security to determine who the body is (was), how they were killed (that should be relatively straightforward, at least), and why (because apparently that matters to a lot of people—who knew?) \n\nYes, the unthinkable is about to happen: Murderbot must voluntarily speak to humans!'
+\set book_6_book_cover_url 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1589475583l/53205854._SY475_.jpg'
+
+\set book_7_title 'Finder'
+\set book_7_title_sort 'finder'
+\set book_7_published_date '2019/04/02'
+\set book_7_edition_date '2019/04/02'
+\set book_7_book_format 'Hardcover'
+\set book_7_total_pages 391
+\set book_7_blurb 'Fergus Ferguson has been called a lot of names: thief, con artist, repo man. He prefers the term finder. \n\nHis latest job should be simple. Find the spacecraft Venetia''s Sword and steal it back from Arum Gilger, ex-nobleman turned power-hungry trade boss. He''ll slip in, decode the ship''s compromised AI security, and get out of town, Sword in hand. \n\nFergus locates both Gilger and the ship in the farthest corner of human-inhabited space, a gas-giant-harvesting colony called Cernee. But Fergus'' arrival at the colony is anything but simple. A cable car explosion launches Cernee into civil war, and Fergus must ally with Gilger''s enemies to navigate a field of space mines and a small army of hostile mercenaries. What was supposed to be a routine job evolves into negotiating a power struggle between factions. Even worse, Fergus has become increasingly--and inconveniently--invested in the lives of the locals. \n\nIt doesn''t help that a dangerous alien species thought mythical prove unsettlingly real, and their ominous triangle ships keep following Fergus around. \n\nFoolhardy. Eccentric. Reckless. Whatever he''s called, Fergus will need all the help he can get to take back the Sword and maybe save Cernee from destruction in the process.'
+\set book_7_book_cover_url 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1543384981l/41795794.jpg'
+
+\set book_8_title 'Driving the Deep'
+\set book_8_title_sort 'driving the deep'
+\set book_8_published_date '2020/05/05'
+\set book_8_edition_date '2020/05/05'
+\set book_8_book_format 'Hardcover'
+\set book_8_total_pages 432
+\set book_8_blurb 'As a professional finder, Fergus Ferguson is hired to locate missing objects and steal them back. But it is rarely so simple, especially after his latest job in Cernee. He''s been recovering from that experience in the company of friends, the Shipmakers of Pluto, experts at crafting top-of-the-line AI spaceships. \n\nThe Shipmakers have convinced Fergus to finally deal with unfinished business he''s been avoiding for half his life: Earth. Fergus hasn''t been back to his homeworld since he was fifteen, when he stole his cousin''s motorcycle and ran away. It was his first theft, and nothing he''s stolen since has been anywhere near so easy, or weighed so heavily on his conscience. Many years and many jobs later, Fergus reluctantly agrees that now is the time to return the motorcycle and face his family. \n\nUnfortunately, someone has gotten to the motorcycle before him. And before he can figure out where it went and why the storage unit that held it is now filled with priceless, stolen art, the Shipyard is attacked. His friends are missing, presumably kidnapped. \n\nAccompanied by an untrustworthy detective who suspects Fergus is the art thief and the sole friend who escaped the attack, Fergus must follow the tenuous clues to locate and save his friends. The trail leads them to Enceladus, where Fergus plans to go undercover to the research stations that lie beneath the moon''s thick ice sheet deep in a dark, oppressive ocean. \n\nBut all movement and personnel are watched, and the limited ways through the thick ice of the moon''s surface are dangerous and highly monitored. Even if Fergus can manage to find proof that his friends are there and alive, getting out again is going to be a lot more complicated than he bargained for.'
+\set book_8_book_cover_url 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1569293574l/53005399._SX318_SY475_.jpg'
+
+
 -- INSERT book using previously declared variables
 SELECT insert_book(:'book_1_title', :'book_1_title_sort', :'book_1_published_date', :'book_1_edition_date', :'book_1_book_format', :'book_1_total_pages', :'book_1_blurb', :'book_1_book_cover_url');
 SELECT insert_book(:'book_2_title', :'book_2_title_sort', :'book_2_published_date', :'book_2_edition_date', :'book_2_book_format', :'book_2_total_pages', :'book_2_blurb', :'book_2_book_cover_url');
 SELECT insert_book(:'book_3_title', :'book_3_title_sort', :'book_3_published_date', :'book_3_edition_date', :'book_3_book_format', :'book_3_total_pages', :'book_3_blurb', :'book_3_book_cover_url');
 SELECT insert_book(:'book_4_title', :'book_4_title_sort', :'book_4_published_date', :'book_4_edition_date', :'book_4_book_format', :'book_4_total_pages', :'book_4_blurb', :'book_4_book_cover_url');
 SELECT insert_book(:'book_5_title', :'book_5_title_sort', :'book_5_published_date', :'book_5_edition_date', :'book_5_book_format', :'book_5_total_pages', :'book_5_blurb', :'book_5_book_cover_url');
+SELECT insert_book(:'book_6_title', :'book_6_title_sort', :'book_6_published_date', :'book_6_edition_date', :'book_6_book_format', :'book_6_total_pages', :'book_6_blurb', :'book_6_book_cover_url');
+SELECT insert_book(:'book_7_title', :'book_7_title_sort', :'book_7_published_date', :'book_7_edition_date', :'book_7_book_format', :'book_7_total_pages', :'book_7_blurb', :'book_7_book_cover_url');
+SELECT insert_book(:'book_8_title', :'book_8_title_sort', :'book_8_published_date', :'book_8_edition_date', :'book_8_book_format', :'book_8_total_pages', :'book_8_blurb', :'book_8_book_cover_url');
 
 
 /* --------------------------------------------------- INSERT reader_book --------------------------------------------------- */
@@ -60,6 +91,9 @@ SELECT insert_reader_book(:'username_1', :'book_2_title');
 SELECT insert_reader_book(:'username_1', :'book_3_title');
 SELECT insert_reader_book(:'username_1', :'book_4_title');
 SELECT insert_reader_book(:'username_1', :'book_5_title');
+SELECT insert_reader_book(:'username_1', :'book_6_title');
+SELECT insert_reader_book(:'username_1', :'book_7_title');
+SELECT insert_reader_book(:'username_1', :'book_8_title');
 
 
 /* --------------------------------------------- INSERT read_instance --------------------------------------------- */
@@ -69,19 +103,40 @@ SELECT insert_read_instance(:'username_1', :'book_2_title');
 SELECT insert_read_instance(:'username_1', :'book_3_title');
 SELECT insert_read_instance(:'username_1', :'book_4_title');
 SELECT insert_read_instance(:'username_1', :'book_5_title');
+SELECT insert_read_instance(:'username_1', :'book_6_title');
+SELECT insert_read_instance(:'username_1', :'book_7_title');
+SELECT insert_read_instance(:'username_1', :'book_8_title');
 
 
 /* --------------------------------------------- INSERT read_entry --------------------------------------------- */
 -- INSERT read_entry data
 -- INSERT book_1
-SELECT insert_read_entry(:'username_1', :'book_1_title', '2021-01-01', 149, :'book_1_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_1_title', '2022-01-01', 149, :'book_1_total_pages');
 -- INSERT book_2
-SELECT insert_read_entry(:'username_1', :'book_2_title', '2021-01-02', 158, :'book_2_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_2_title', '2022-01-02', 158, :'book_2_total_pages');
 -- INSERT book_3
-SELECT insert_read_entry(:'username_1', :'book_3_title', '2021-01-03', 158, :'book_3_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_3_title', '2022-01-03', 158, :'book_3_total_pages');
 -- INSERT book_4
-SELECT insert_read_entry(:'username_1', :'book_4_title', '2021-01-04', 176, :'book_4_total_pages');
-
+SELECT insert_read_entry(:'username_1', :'book_4_title', '2022-01-04', 176, :'book_4_total_pages');
+-- INSERT book_5
+SELECT insert_read_entry(:'username_1', :'book_5_title', '2022-01-04', 56, :'book_5_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_5_title', '2022-01-05', 108, :'book_5_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_5_title', '2022-01-06', 163, :'book_5_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_5_title', '2022-01-07', 234, :'book_5_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_5_title', '2022-01-08', 350, :'book_5_total_pages');
+-- INSERT book_6
+SELECT insert_read_entry(:'username_1', :'book_6_title', '2022-01-09', 168, :'book_6_total_pages');
+-- INSERT book_7
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-17', 46, :'book_7_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-18', 98, :'book_7_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-19', 156, :'book_7_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-20', 200, :'book_7_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-21', 245, :'book_7_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-22', 302, :'book_7_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_7_title', '2022-01-23', 400, :'book_7_total_pages');
+-- INSERT book_8
+SELECT insert_read_entry(:'username_1', :'book_8_title', '2022-01-27', 64, :'book_8_total_pages');
+SELECT insert_read_entry(:'username_1', :'book_8_title', '2022-01-28', 120, :'book_8_total_pages');
 
 /* --------------------------------------------- INSERT author --------------------------------------------- */
 -- INSERT author and INSERT book_author tables
@@ -90,16 +145,22 @@ SELECT insert_author(:'book_2_title', 'Martha Wells');
 SELECT insert_author(:'book_3_title', 'Martha Wells');
 SELECT insert_author(:'book_4_title', 'Martha Wells');
 SELECT insert_author(:'book_5_title', 'Martha Wells');
+SELECT insert_author(:'book_6_title', 'Martha Wells');
+SELECT insert_author(:'book_7_title', 'Suzanne Palmer');
+SELECT insert_author(:'book_8_title', 'Suzanne Palmer');
 
 
 /* --------------------------------------------- UPDATE read_instance --------------------------------------------- */
 -- UPDATE read_instance.is_finished to true
 SELECT update_read_instance(:'username_1', :'book_1_title', FALSE, TRUE, FALSE);
 SELECT update_read_instance(:'username_1', :'book_2_title', FALSE, TRUE, FALSE);
+SELECT update_read_instance(:'username_1', :'book_3_title', FALSE, TRUE, FALSE);
+SELECT update_read_instance(:'username_1', :'book_4_title', FALSE, TRUE, FALSE);
+SELECT update_read_instance(:'username_1', :'book_5_title', FALSE, TRUE, FALSE);
+SELECT update_read_instance(:'username_1', :'book_6_title', FALSE, TRUE, FALSE);
+SELECT update_read_instance(:'username_1', :'book_7_title', FALSE, TRUE, FALSE);
 -- UPDATE read_instance.is_reading to true
-SELECT update_read_instance(:'username_1', :'book_3_title', TRUE, FALSE, FALSE);
-SELECT update_read_instance(:'username_1', :'book_4_title', TRUE, FALSE, FALSE);
-SELECT update_read_instance(:'username_1', :'book_5_title', TRUE, FALSE, FALSE);
+SELECT update_read_instance(:'username_1', :'book_8_title', TRUE, FALSE, FALSE);
 
 
 /* --------------------------------------------- UPDATE reader_book  --------------------------------------------- */
@@ -108,3 +169,6 @@ SELECT update_reader_book(:'username_1', :'book_2_title');
 SELECT update_reader_book(:'username_1', :'book_3_title');
 SELECT update_reader_book(:'username_1', :'book_4_title');
 SELECT update_reader_book(:'username_1', :'book_5_title');
+SELECT update_reader_book(:'username_1', :'book_6_title');
+SELECT update_reader_book(:'username_1', :'book_7_title');
+SELECT update_reader_book(:'username_1', :'book_8_title');
