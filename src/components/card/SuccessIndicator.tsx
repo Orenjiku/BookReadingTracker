4dd3aec432c642dd83f3ee6a7232c8239d1f4318
William Chang <wdchang86@gmail.com>
2022-01-28 17:29:38 -0500

    Refactored/simplified SuccessIndicator.

--- src/components/card/SuccessIndicator.tsx
+++ src/components/card/SuccessIndicator.tsx
@@ -3,37 +3,30 @@ import tw, { styled, css } from 'twin.macro';
 import { CgCheckO, CgCloseO } from 'react-icons/cg';
 
 
-const CgCheckContainer = styled.div<{$isSuccess: boolean; $indicatorTransitionTimer: number}>`
+const IndicatorContainer = styled.div<{ $indicatorTrigger: boolean; $indicatorTransitionTimer: number }>`
   ${tw`absolute overflow-hidden`};
   backface-visibility: hidden;
   transform: rotateX(-180deg);
   transform-origin: center;
   --flipDuration: ${({ $indicatorTransitionTimer }) => `${$indicatorTransitionTimer}ms`};
   transition: transform var(--flipDuration) linear;
-  ${({ $isSuccess}) => $isSuccess && css`
+  ${({ $indicatorTrigger}) => $indicatorTrigger && css`
     transform: rotateX(0);
-  `}
-`
-
-const CgCloseContainer = styled.div<{$isFail: boolean; $indicatorTransitionTimer: number}>`
-  ${tw`absolute overflow-hidden`};
-  backface-visibility: hidden;
-  transform: rotateX(-180deg);
-  transform-origin: center;
-  --flipDuration: ${({ $indicatorTransitionTimer }) => `${$indicatorTransitionTimer}ms`};
-  transition: transform var(--flipDuration) linear;
-  ${({ $isFail}) => $isFail && css`
-    transform: rotateX(0deg);
   `}
 `
 
 const SuccessIndicator = ({size, isSuccess, isFail, indicatorTransitionTimer}: {size: number; isSuccess: boolean; isFail: boolean; indicatorTransitionTimer: number}) => {
   return (
     <div className='relative flex items-center' style={{transformStyle: 'preserve-3d'}}>
-      <CgCheckContainer $isSuccess={isSuccess} $indicatorTransitionTimer={indicatorTransitionTimer}><CgCheckO size={size} className='text-green-600' /></CgCheckContainer>
-      <CgCloseContainer $isFail={isFail} $indicatorTransitionTimer={indicatorTransitionTimer}><CgCloseO size={size} className='text-red-600'/></CgCloseContainer>
+      <IndicatorContainer $indicatorTrigger={isSuccess} $indicatorTransitionTimer={indicatorTransitionTimer}>
+        <CgCheckO size={size} className='text-green-600' />
+      </IndicatorContainer>
+
+      <IndicatorContainer $indicatorTrigger={isFail} $indicatorTransitionTimer={indicatorTransitionTimer}>
+        <CgCloseO size={size} className='text-red-600'/>
+      </IndicatorContainer>
     </div>
   )
-}
+};
 
 export default SuccessIndicator;
